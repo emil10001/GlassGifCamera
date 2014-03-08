dd5b26059ca01c9b30fea19cab879a87be5c7c64
E John Feig <ejohn@feigdev.com>
2014-03-08 11:34:25 -0800

    fixed voice launch bug

--- src/main/java/com/feigdev/glassgif/PhotoFrag.java
+++ src/main/java/com/feigdev/glassgif/PhotoFrag.java
@@ -3,7 +3,9 @@ package com.feigdev.glassgif;
 import android.app.Fragment;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,9 +18,9 @@ import android.widget.FrameLayout;
 public class PhotoFrag extends Fragment {
     private static final String TAG = "PhotoFrag";
     private Camera camera;
-    private int cameraId = 0;
     private CameraPreview cameraPreview;
     private FrameLayout preview;
+    private Handler handler = new Handler();
 
     public PhotoFrag() {
     }
@@ -26,31 +28,39 @@ public class PhotoFrag extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.d(TAG,"onCreateView");
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        cameraId = findFrontFacingCamera();
         preview = (FrameLayout) rootView.findViewById(R.id.preview);
 
         return rootView;
     }
 
-    private int findFrontFacingCamera() {
-        int cameraId = -1;
-        // Search for the front facing camera
-        int numberOfCameras = Camera.getNumberOfCameras();
-        for (int i = 0; i < numberOfCameras; i++) {
-            Camera.CameraInfo info = new Camera.CameraInfo();
-            Camera.getCameraInfo(i, info);
-            Log.d(TAG, "Camera found");
-            cameraId = i;
-            break;
-        }
-        return cameraId;
+    @Override
+    public void onResume() {
+        Log.d(TAG,"onResume");
+        super.onResume();
+
+        new GlassPhotoDelay().execute();
+
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
+    public void onPause() {
+        Log.d(TAG,"onPause");
+        preview.removeView(cameraPreview);
+
+        if (camera != null) {
+            camera.stopPreview();
+            camera.release();
+            camera = null;
+        }
+        super.onPause();
+    }
+
+
+    private void initCamera(){
+        Log.d(TAG,"initCamera");
 
         // do we have a camera?
         if (!getActivity().getPackageManager()
@@ -59,12 +69,7 @@ public class PhotoFrag extends Fragment {
             return;
         }
 
-        if (cameraId < 0) {
-            Log.d(TAG, "No front facing camera found.");
-            return;
-        }
-
-        camera = Camera.open(cameraId);
+        camera = Camera.open();
 
         // SO help: http://stackoverflow.com/a/19257078/974800
         Camera.Parameters params = camera.getParameters();
@@ -74,9 +79,12 @@ public class PhotoFrag extends Fragment {
         cameraPreview = new CameraPreview(getActivity(), camera);
         preview.addView(cameraPreview);
 
+    }
 
-//        camera.takePicture(null, null,
-//                new PhotoHandler());
+    private void takePictures(){
+
+        camera.takePicture(null, null,
+                new PhotoHandler());
 
 //        for (int i = 0; i < 5; i++) {
 //            Log.d(TAG, "take picture " + i);
@@ -86,14 +94,24 @@ public class PhotoFrag extends Fragment {
 
     }
 
-    @Override
-    public void onPause() {
-        preview.removeView(cameraPreview);
 
-        if (camera != null) {
-            camera.release();
-            camera = null;
+    private class GlassPhotoDelay extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            Log.d(TAG, "GlassPhotoDelay");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
         }
-        super.onPause();
+
+        @Override
+        protected void onPostExecute(Void params) {
+            initCamera();
+            takePictures();
+        }
     }
 }
